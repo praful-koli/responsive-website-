07999a34bd557f40494c35a4033a5f5cb35cba00
praful-koli <prafulkoli852@gmail.com>
2025-10-26 20:14:01 +0530

    update code

---
+++ app.js
@@ -0,0 +1,28 @@
+import Lenis from '@studio-freight/lenis'
+// Initialize Lenis
+// const lenis = new Lenis()
+
+// lenis.on('scroll', (e) => {
+//   console.log(e)
+// })
+
+// function raf(time) {
+//   lenis.raf(time)
+//   requestAnimationFrame(raf)
+// }
+
+// requestAnimationFrame(raf)
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+lenis.on('scroll', ScrollTrigger.update)
+
+gsap.ticker.add((time)=>{
+  lenis.raf(time * 1000)
+})
+
+gsap.ticker.lagSmoothing(3)
